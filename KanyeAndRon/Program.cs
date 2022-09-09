38f6e7cc3a3a99bd74e3c7f47d5810c20e1bf16f
maul49zer@gmail.com <maul49zer@gmail.com>
2022-09-09 13:58:24 -0600

    Add project files.

---
+++ KanyeAndRon/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using KanyeAndRon;
+using Newtonsoft.Json.Linq;
+
+for (int i = 0; i < 5; i++)
+{
+    QuoteGenerator.KanyeQuote();
+    QuoteGenerator.RonQuote();
+}
+
+
+
+
+
+
---
+++ KanyeAndRon/QuoteGenerator.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanyeAndRon
+{
+    internal class QuoteGenerator
+    {
+        public static void KanyeQuote()
+        {
+            var client = new HttpClient();
+            var kanyeResponse = client.GetStringAsync("https://api.kanye.rest/").Result;
+            var kanyeQuote = JObject.Parse(kanyeResponse);
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine($"Kanye:  '{kanyeQuote["quote"]}'");
+            Console.WriteLine("---------------------------------------");
+
+        }
+
+        public static void RonQuote()
+        {
+            var client = new HttpClient();
+            var ronResponse = client.GetStringAsync("https://ron-swanson-quotes.herokuapp.com/v2/quotes").Result;
+            var ronQuote = JArray.Parse(ronResponse);
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine($"Ron:  '{ronQuote[0]}'");
+            Console.WriteLine("---------------------------------------");
+        }
+    }
+}
